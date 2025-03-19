125f23c08057b823aa4946c643832b32aaeda688
Douglas Jasper <doug.jasper@trios.com>
2025-03-19 08:13:36 -0400

    Initial Commit

---
+++ PopOver/PopOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopOverApp.swift
+//  PopOver
+//
+//  Created by Douglas Jasper on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct PopOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
